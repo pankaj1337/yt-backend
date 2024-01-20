0103c3b9c2236679894118e622b557ee45d98cb0
pankaj kumar <scoutpankaj037@gmail.com>
2024-01-20 16:28:48 +0530

    another commit

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,10 +1,21 @@
 import {asyncHandler } from "../utils/asyncHandler.js"
-
+import {ApiError} from "../utils/ApiError.js"
 
 const registerUser = asyncHandler(async (req, res)=>{
-    res.status(200).json({
-        message:"ok"
-    })
+   //get user details form frontend
+   //validation - not empty
+   //check if user exists : username , email
+   //check for images, check for avatar
+   //upload them to cloudinary
+   //remove password and refresh token field from response
+   //check for user creation
+   //return res
+
+   const { username, email, password, avatar } = req.body;
+   console.log("email: ", email)
+   if (fullName ===""){
+      throw new ApiError(400, "full name is required")
+   }
 })
 
 
